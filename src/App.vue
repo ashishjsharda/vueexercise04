3b3429ebb4af33dae0da2149c6dd60be16d7782b
Ashish Sharda <ashishjsharda@gmail.com>
2022-10-15 06:52:12 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,32 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss" module>
+h1,
+
+h2 {
+
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
   text-align: center;
+
+}
+
+.title {
+
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+
   color: #2c3e50;
+
   margin-top: 60px;
+
+}
+
+.subtitle {
+
+  color: #4fc08d;
+
+  font-style: italic;
+
 }
 </style>
